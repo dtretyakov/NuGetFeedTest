bfee600a1a67dd5952cfa9425d7f623524a1ff0d
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-02-09 12:20:54 +0100

    add initial rider project

---
+++ NuGetFeedTest/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NuGetFeedTest
+{
+    public class Calculator
+    {
+        public double plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ TestNuGetFeed/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestNuGetFeed
+{
+    [TestFixture]
+    public class TestLocalTeamCityFeed
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
