b5e7269d2c826fab9c96544b01ddd8fb66ebc681
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-02-21 19:05:22 +0100

    add new project

---
+++ NuGetPackage1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuGetPackage1
+{
+    public static class Class1
+    {
+        public static Boolean AlwaysTrue()
+        {
+            return true;
+        }
+    }
+}
